9c7c1d9020b2b63095d4119e6b6532316a7547ee
David Warring <david.warring@gmail.com>
2013-05-18 05:27:34 +1000

    values and units: recombine sources. grammar for calc(), attr() & toggle()

--- lib/CSS/Language/CSS3/Values_and_Units.pm
+++ lib/CSS/Language/CSS3/Values_and_Units.pm
@@ -3,16 +3,43 @@ use v6;
 # This class implements CSS3 Values and Units Module Level 3
 # - reference: http://www.w3.org/TR/2013/CR-css3-values-20130404/
 #
+class CSS::Language::CSS3::Values_and_Units::Actions {...}
 
-use CSS::Language::CSS3::Values::Units; # additional units
-use CSS::Language::CSS3::Values::Funcs;  # calc(), attr() ...
+use CSS::Language::CSS3::_Base;
 
 grammar CSS::Language::CSS3::Values_and_Units
-    is CSS::Language::CSS3::Values::Units
-    is CSS::Language::CSS3::Values::Funcs
-{};
+    is CSS::Language::CSS3::_Base {
+
+    # -- Units -- #
+
+    # add viewport as a new units family
+    token distance-units:sym<rel-viewport> {:i vw|vh|vmin|vmax}
+
+    # override/extend css::grammar <rel-font-units> & <angle-units>
+    token rel-font-units {:i[em|ex|ch|rem]}
+    token angle-units    {:i[deg|rad|rad|turn]}
+    # override/extend css::language::css3::_base <resolution-units>
+    token resolution-units {:i[dpi|dpcm|dppx]}
+
+    # -- Functions -- #
+
+    token op     {<punct>}
+    rule math    {<calc> }
+    rule calc    {:i 'calc(' <sum> ')' }
+    rule sum     { <product> +% [['+'|'-'] & <op> ] } 
+    rule product { <unit> [ ['*' & <op>] <unit> | ['/' & <op>] <number> ]* }
+    rule attr    {:i 'attr(' <qname> <type-or-unit>? [ ',' [ <unit> | <calc> ] ]? ')' }
+    rule unit    { <number> | <dimension> | <percentage> | '(' <sum> ')' | <calc> | <attr> }
+
+    rule toggle {:i 'toggle(' <term> +% [ ',' ] ')' }
+
+    token unit-name {<angle-units>|<distance-units>|<rel-font-units>|<resolution-units>}
+    token type-or-unit {:i [string|color|url|integer|number|length|angle|time|frquency] & <keyw> | <unit-name> }
+
+
+};
 
 class CSS::Language::CSS3::Values_and_Units::Actions
-    is CSS::Language::CSS3::Values::Units::Actions
-    is CSS::Language::CSS3::Values::Funcs::Actions
-{};
+    is CSS::Language::CSS3::_Base::Actions {
+
+};
