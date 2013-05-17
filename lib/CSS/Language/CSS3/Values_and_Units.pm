b412f9667d1de904d1876a7e6f7a75453812f814
David Warring <david.warring@gmail.com>
2013-05-18 07:53:38 +1000

    continuing work on values and units - math

--- lib/CSS/Language/CSS3/Values_and_Units.pm
+++ lib/CSS/Language/CSS3/Values_and_Units.pm
@@ -21,25 +21,48 @@ grammar CSS::Language::CSS3::Values_and_Units
     # override/extend css::language::css3::_base <resolution-units>
     token resolution-units {:i[dpi|dpcm|dppx]}
 
-    # -- Functions -- #
+    # -- Math -- #
 
+    rule math    { <calc> | <attr> | <toggle> }
     token op     {<punct>}
-    rule math    {<calc> }
     rule calc    {:i 'calc(' <sum> ')' }
-    rule sum     { <product> +% [['+'|'-'] & <op> ] } 
+    rule sum     { <product> +% [ ['+'|'-'] & <op> ] } 
     rule product { <unit> [ ['*' & <op>] <unit> | ['/' & <op>] <number> ]* }
     rule attr    {:i 'attr(' <qname> <type-or-unit>? [ ',' [ <unit> | <calc> ] ]? ')' }
     rule unit    { <number> | <dimension> | <percentage> | '(' <sum> ')' | <calc> | <attr> }
 
+    token unit-name {<angle-units>|<distance-units>|<rel-font-units>|<resolution-units>}
+
+    token type-or-unit {:i [string|color|url|integer|number|length|angle|time|frequency] & <keyw>
+                                | <unit-name> }
+
     rule toggle {:i 'toggle(' <term> +% [ ',' ] ')' }
 
-    token unit-name {<angle-units>|<distance-units>|<rel-font-units>|<resolution-units>}
-    token type-or-unit {:i [string|color|url|integer|number|length|angle|time|frquency] & <keyw> | <unit-name> }
+    # extend language grammars
+    token length:sym<math>     {<math>}
+    token angle:sym<math>      {<math>}
+    token frequency:sym<math>  {<math>}
+    token time:sym<math>       {<math>}
+    token resolution:sym<math> {<math>}
+    token color:sym<math>      {<math>}
+    # todo: strings urls
+    # todo: handle keyword toggling - ideally without complicating
+    # property declarations.
+    rule keyw-toggle           {:i 'toggle(' <ident> +% [ ',' ] }
 
-
+    # override misc rule to catch and eventually handle toggled keywords
+    rule misc                   {<keyw-toggle> | <proforma>**0..1 <any-arg>*}
 };
 
 class CSS::Language::CSS3::Values_and_Units::Actions
     is CSS::Language::CSS3::_Base::Actions {
+
+    method misc($/) {
+        return $/.warn('tba - toggle(...) on keywords')
+            if $<keyw-toggle>;
+
+        make $<proforma>[0].ast
+            if $<proforma> && !$<any-args>;
+    }
 
 };
