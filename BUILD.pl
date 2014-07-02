55b1441ad9faa765d30eb497e196583939ec62b9
David Warring david.warring@gmail.com <david.warring@gmail.com>
2014-07-01 18:16:25 -0700

    initial css::spec generation

---
+++ BUILD.pl
@@ -0,0 +1,33 @@
+use v6;
+
+multi MAIN( Bool :$interfaces=True, Bool :$grammars=True, Bool :$actions=True ) {
+    for (
+         'etc/css3x-background-20120724.txt' => <CSS3 Backgrounds_and_Borders>,
+        ) {
+        my ($spec, $class-isa) = .kv;
+
+        my @productions;
+
+        @productions.push: 'role'    => 'Interface'
+            if $interfaces;
+
+        @productions.push: 'class'   => 'Actions'
+            if $actions;
+
+        @productions.push: 'grammar' => 'Grammar'
+            if $grammars;
+
+        for @productions {
+            my ($opt, $subclass) = .kv;
+            my $flags = $opt eq 'grammar' ?? ' --proforma' !! '';
+            my $class-name = (<CSS Language>, @$class-isa, <Spec>,  $subclass).join('::');
+            my $class-path = (<lib CSS Language>, @$class-isa, <Spec>, $subclass).join('/');
+            my $perl6 = $*EXECUTABLE_NAME;
+            # See CSS::Specification
+            my $cmd = "perl6-m `which css-gen-properties.pl` --{$opt}={$class-name}$flags $spec > {$class-path}.pm";
+            say $cmd;
+            shell $cmd;
+        }
+    }
+}
+     
