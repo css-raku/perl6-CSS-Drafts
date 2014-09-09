dbe56fbf06a0d4d49e1344ae104275af9c1fda9c
David Warring david.warring@gmail.com <david.warring@gmail.com>
2014-09-09 14:18:29 -0700

    [etc, Backgrounds_and_Borders.pm] rationalize rules

--- lib/CSS/Module/CSS3/Backgrounds_and_Borders.pm
+++ lib/CSS/Module/CSS3/Backgrounds_and_Borders.pm
@@ -12,18 +12,11 @@ use CSS::Module::CSS3::Backgrounds_and_Borders::Spec::Actions;
 grammar CSS::Module::CSS3::Backgrounds_and_Borders:ver<20120724.000>
     is CSS::Module::CSS3::Backgrounds_and_Borders::Spec::Grammar {
 
-    rule attachment   {:i [ scroll | fixed | local ] & <keyw> }
     rule image        {<uri>}
-    rule bg-image     {:i <image> | none & <keyw> }
     rule box          {:i [  border\-box | padding\-box | content\-box ] & <keyw> }
-    rule repeat-style {:i [ repeat\-x | repeat\-y ] & <keyw>
-                           | [ [repeat | space | round | no\-repeat] & <keyw> ] ** 1..2 }
-    rule bg-layer {:i [ <bg-image> | <position> [ '/' <bg-size> ]? | <repeat-style> | <attachment> | <box>**1..2 ] ** 1..5 }
+    rule bg-layer {:i [:my @*SEEN; <bg-image=.expr-background-image> <!seen(0)> | <position> [ '/' <bg-size> ]? <!seen(1)> | <repeat-style=.expr-background-repeat> <!seen(2)> | <attachment> <!seen(3)> | <box=.expr-background-clip>**1..2 <!seen(4)> ]+ }
     rule final-bg-layer { [ <bg-layer> | <color> ] ** 1..2}
     rule position {:i [ <percentage> | <length> | [ [ left | center | right | top | bottom ] & <keyw> ] [ <percentage> | <length> ] ? ] ** 1..2 }
-    rule bg-size {:i [ <length> | <percentage> | auto & <keyw> ] ** 1..2 | [cover | contain ] & <keyw> }
-    rule border-style {:i [ none | hidden | dotted | dashed | solid | double | groove | ridge | inset | outset ] & <keyw> }
-    rule border-width {:i <length> | [ thin | medium | thick ] & <keyw> }
     rule shadow {:i [ <length> | <color> | inset & <keyw> ]+ }
 }
 
@@ -31,17 +24,11 @@ class CSS::Module::CSS3::Backgrounds_and_Borders::Actions
     is CSS::Module::CSS3::Backgrounds_and_Borders::Spec::Actions {
 
     method image($/) { make $<uri>.ast }
-    method bg-image($/) {  make $.list($/) }
     method box($/) { make $.token($<keyw>.ast) }
-    method repeat-style($/) { make $.list($/)}
     method bg-layer($/) { make $.list($/) }
     method final-bg-layer($/) { make $.list($/) }
 
-    method attachment($/) { make $.list($/) }
     method position($/) { make $.list($/) }
-    method bg-size($/) { make $.list($/) }
-    method border-style($/) { make $.list($/) }
-    method border-width($/) { make $.list($/) }
     method shadow($/) { make $.list($/) }
 
 }
