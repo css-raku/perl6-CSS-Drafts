7c5975e0556c2b5464c34ab77fb329a807f785a2
David Warring david.warring@gmail.com <david.warring@gmail.com>
2014-10-30 13:46:10 -0700

    support CSSValue standard types

--- lib/CSS/Module/CSS3/Values_and_Units.pm
+++ lib/CSS/Module/CSS3/Values_and_Units.pm
@@ -5,6 +5,7 @@ use v6;
 #
 class CSS::Module::CSS3::Values_and_Units::Actions {...}
 use CSS::Module::CSS3::_Base;
+use CSS::Grammar::AST :CSSValue;
 
 grammar CSS::Module::CSS3::Values_and_Units
     is CSS::Module::CSS3::_Base {
@@ -41,8 +42,8 @@ grammar CSS::Module::CSS3::Values_and_Units
 
     # implement proforma rules for attr() and toggle()
     # - see <val> rule in CSS::Specification::_Base.
-    rule toggle-val {<val($*EXPR, $*USAGE)>}
-    rule toggle     {:i 'toggle(' <toggle-val> +% [ ',' ] ')' }
+    rule toggle-val { <val($*EXPR, $*USAGE)> }
+    rule toggle     {:i 'toggle(' <toggle-val> +% ',' ')' }
     rule attr       {:i 'attr(' <attr-name=.qname> [<type>|<unit-name>]? [ ',' <val($*EXPR, $*USAGE)> ]? ')' }
     rule proforma:sym<toggle> { <toggle> }
     rule proforma:sym<attr>   { <attr> }
@@ -51,22 +52,21 @@ grammar CSS::Module::CSS3::Values_and_Units
 class CSS::Module::CSS3::Values_and_Units::Actions
     is CSS::Module::CSS3::_Base::Actions {
 
-    method distance-units:sym<viewport>($/) { make $.token( (~$/).lc, :type<length> ) }
-    method rel-font-units($/)               { make $.token( (~$/).lc, :type<length> ) }
-    method angle-units($/)                  { make $.token( (~$/).lc, :type<angle> ) }
-    method resolution-units($/)             { make $.token( (~$/).lc, :type<resolution> ) }
+    method distance-units:sym<viewport>($/) { make $.token( $/.lc, :type(CSSValue::LengthComponent) ) }
+    method rel-font-units($/)               { make $.token( $/.lc, :type(CSSValue::LengthComponent) ) }
+    method angle-units($/)                  { make $.token( $/.lc, :type(CSSValue::AngleComponent) ) }
+    method resolution-units($/)             { make $.token( $/.lc, :type(CSSValue::ResolutionComponent) ) }
 
     method math($/) { make $.token( $.node($/), :type( $<calc>.ast.type )) }
 
     method _coerce-types($lhs, $rhs) {
         return do {
-            when $lhs eq $rhs             {$lhs}
-            when $lhs eq 'percentage'     {$rhs} 
-            when $rhs eq 'percentage'     {$lhs} 
-            when ($lhs, $rhs).sort
-                eqv ('integer', 'number') {'number'}
+            when $lhs eq $rhs                          {$lhs}
+            when $lhs eq CSSValue::PercentageComponent {$rhs} 
+            when $rhs eq CSSValue::PercentageComponent {$lhs} 
+            when ($lhs, $rhs) (>=) (CSSValue::IntegerComponent, CSSValue::NumberComponent) {CSSValue::NumberComponent}
  
-            default {'incompatible'}
+            default {Any}
         }
     }
 
@@ -80,26 +80,30 @@ class CSS::Module::CSS3::Values_and_Units::Actions
 
     multi method _resolve-op-type('*', $lhs, $rhs) {
 
-        my $l-int = $lhs eq 'integer';
-        my $r-int = $rhs eq 'integer';
+        my $Int = CSSValue::IntegerComponent;
+        my $Num = CSSValue::NumberComponent;
+
+        my $l-int = $lhs eq $Int;
+        my $r-int = $rhs eq $Int;
 
         return do {
-           when $l-int && $r-int {'integer'}
+           when $l-int && $r-int {$Int}
 
-           my $l-num = $l-int || $lhs eq 'number';
-           my $r-num = $r-int || $rhs eq 'number';
+           my $l-num = $l-int || $lhs eq $Num;
+           my $r-num = $r-int || $rhs eq $Num;
 
-           when $l-num && $r-num {'number'}
+           when $l-num && $r-num {$Num}
            when $r-num           {$lhs}
            when $l-num           {$rhs}
-           default               {'incompatible'}
+           default               {Any}
         }
     }
 
     multi method _resolve-op-type('/', $lhs, $rhs) {
         die "lhs of '/' has type {$rhs} - expected number or integer"
-           unless $rhs eq 'number' || $rhs eq 'integer';
-        return 'number' if $lhs eq 'integer';
+           unless $rhs eq CSSValue::NumberComponent | CSSValue::IntegerComponent;
+        return CSSValue::NumberComponent
+            if $lhs eq CSSValue::IntegerComponent;
         return $lhs;
     }
 
@@ -153,41 +157,41 @@ class CSS::Module::CSS3::Values_and_Units::Actions
 
         my $type = $<type> && $<type>.ast;
         $type //= $<unit-name> && lc $<unit-name>;
-        $type //= 'string';
+        $type //= CSSValue::StringComponent;
 
         make $.token( %ast, :type($type));
     }
 
     method proforma:sym<toggle>($/) { make $.node($/) }
-    method proforma:sym<attr>($/) { make $.node($/) }
+    method proforma:sym<attr>($/)   { make $.node($/) }
 
     method attr-expr($/) {
         my $expr = $.list($/);
         my $type = $<type> && $<type>.ast;
         $type //= $<unit-name> && $<unit-name>.ast.type;
-        $type //= 'string';
+        $type //= CSSValue::StringComponent;
         make $.token( $expr, :type($type));
     }
     method unit($/) {
         my $item = $/.caps[0].value.ast;
         my $type = $item.type
-            // ($item.units eq '%' && 'percentage');
+            // ($item.units eq '%' && CSSValue::PercentageComponent);
         make $.token( $.node($/), :type($type) );
     }
     method type($/)      { make $<keyw>.ast }
     method unit-name($/) { make $<units>.ast }
 
-    method _grok-expr($expr, $base-type) {
+    method _type-check-expr($expr, $base-type) {
         my $expr-ast = $expr.ast;
 
         my $expr-type = $expr-ast.type;
-        if $expr-type eq 'incompatible' {
+        unless $expr-type.defined {
             $.warning("incompatible types in expression", ~$/);
             return Any;
         }
 
         my $type = $._coerce-types($base-type, $expr-type);
-        if $type eq 'incompatible' {
+        unless $type.defined {
             $.warning("expected an expresssion of type {$base-type}, got: {$expr-type}", ~$expr);
             return Any;
         }
@@ -196,10 +200,10 @@ class CSS::Module::CSS3::Values_and_Units::Actions
         return $expr-ast;
     }
 
-    method length:sym<math>($/)     { make $._grok-expr($<math>, <length>); }
-    method frequency:sym<math>($/)  { make $._grok-expr($<math>, <frequency>); }
-    method angle:sym<math>($/)      { make $._grok-expr($<math>, <angle>); }
-    method time:sym<math>($/)       { make $._grok-expr($<math>, <time>); }
-    method resolution:sym<math>($/) { make $._grok-expr($<math>, <resolution>); }
+    method length:sym<math>($/)     { make $._type-check-expr($<math>, CSSValue::LengthComponent); }
+    method frequency:sym<math>($/)  { make $._type-check-expr($<math>, CSSValue::FrequencyComponent); }
+    method angle:sym<math>($/)      { make $._type-check-expr($<math>, CSSValue::AngleComponent); }
+    method time:sym<math>($/)       { make $._type-check-expr($<math>, CSSValue::TimeComponent); }
+    method resolution:sym<math>($/) { make $._type-check-expr($<math>, CSSValue::ResolutionComponent); }
 
 };
